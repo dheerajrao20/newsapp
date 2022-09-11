24c608f15e2b740c3496bc857d1f444eea665187
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-09-11 09:27:31 +0530

    adding components

---
+++ src/components/Newsitem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Newsitem extends Component {
+  render() {
+    return (
+      <div>
+        this is item
+      </div>
+    )
+  }
+}
+
+export default Newsitem
